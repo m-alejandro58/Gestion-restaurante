1008daf5ed4b578f840e0d26caff1862d924febf
Manuel Alejandro Alvarez <alejandroalvare401@gmail.com>
2026-05-25 14:59:29 -0500

    Create products form in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,13 @@ import axios from "axios";
 function App() {
   const [products, setProducts] = useState([]);
 
+  const [formData, setFormData] = useState({
+    name: "",
+    price: "",
+    category: "",
+    stock: ""
+  });
+
   const getProducts = async () => {
     try {
       const response = await axios.get(
@@ -20,9 +27,88 @@ function App() {
     getProducts();
   }, []);
 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post(
+        "http://localhost:5000/api/products",
+        formData
+      );
+
+      setFormData({
+        name: "",
+        price: "",
+        category: "",
+        stock: ""
+      });
+
+      getProducts();
+    } catch (error) {
+      console.error("Error creando producto");
+    }
+  };
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>Gestión Restaurante</h1>
+
+      <h2>Agregar Producto</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nombre"
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="number"
+          name="price"
+          placeholder="Precio"
+          value={formData.price}
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="text"
+          name="category"
+          placeholder="Categoría"
+          value={formData.category}
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <input
+          type="number"
+          name="stock"
+          placeholder="Stock"
+          value={formData.stock}
+          onChange={handleChange}
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Guardar Producto
+        </button>
+      </form>
+
+      <hr />
 
       <h2>Productos</h2>
 
